ef54b90fcf98798fc06d8cd2d625da51f0c75d6d
Mohammed Benchelbikh <mohammed.benchelbikh@usmba.ac.ma>
2020-12-22 12:04:01 +0100

    Location

---
+++ Location.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace diagramme_classe
+{
+    public class Location
+    {
+        private string _name;
+        public string name { get => _name; set { _name = value; } }
+
+
+        public Location(string n) => name = n;
+
+        public override string ToString() => $"location : {name}";
+
+
+    }
+}
