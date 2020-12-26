e4f0387bbdf808d1dead101e62fafa109b17ee79
Mohammed Benchelbikh <mohammed.benchelbikh@usmba.ac.ma>
2020-12-26 17:13:21 +0100

    get all citoyens from database

---
+++ Controllers/CitoyenController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class CitoyenController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        
+        public CitoyenController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        } 
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string cnxS = _configuration.GetConnectionString("CitoyenAppCon");
+            DataTable table = new DataTable();
+            using (SqlConnection cnx = new SqlConnection(cnxS))
+            {
+                string request = "Select * from citoyens";
+                cnx.Open();
+                
+                using (SqlCommand cmd = new SqlCommand(request, cnx))
+                {
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    table.Load(reader);
+
+                    reader.Close();
+                    cnx.Close();
+                }
+            }
+
+            return new JsonResult(table);
+        }
+    }
+}
---
+++ Models/Citoyen.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public enum Color
+    {
+        red,
+        green,
+        orange,
+        blue,
+        gray
+    }
+
+    public class Citoyen
+    {
+        
+        public string cin { get; set; }
+        
+        
+        private Color codeCouleur
+        {
+            get {
+                if (CheckType() == "Patient") return Color.red;
+                if (isSuspect) return Color.orange;
+                return Color.green;
+            }
+        }        
+        public bool isVaccinated { get; set; }
+        public bool inQuarantine { get; set; }
+        public bool isSuspect { get; set; }
+
+        public string CheckType()
+        {
+            return GetType().ToString();
+        }
+
+        public List<Citoyen> cotoye
+        {
+            get
+            {
+                List<Citoyen> _cotoye;
+                if (graph.TryGetValue(cin, out _cotoye))
+                {
+                    return _cotoye;
+                }
+                return new List<Citoyen>();
+            }
+        }
+        public Citoyen(string cin)
+        {
+
+
+            try
+            {
+                graph.Add(cin, new List<Citoyen>());
+                this.cin = cin;
+                isSuspect = false;
+                isVaccinated = false;
+                inQuarantine = false;
+
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("An element with Key = {0} already exists.", cin);
+            }
+
+
+        }
+        protected Citoyen(Citoyen c)
+        {
+            foreach (var cit in c.cotoye)
+            {
+                cit.isSuspect = true;
+            }
+
+            cin = c.cin;
+            isVaccinated = c.isVaccinated;
+            inQuarantine = c.inQuarantine;
+        }
+        //public T Clone<T>() where T : Citoyen, new()
+        //{
+        //    return new T() { cin = this.cin, codeCouleur = this.codeCouleur , isVaccinated = isVaccinated , inQuarantine = inQuarantine };
+        //}
+        public void SetCitoyenB(List<Citoyen> newCitoyenB)
+        {
+            RemoveAllBeenInContactWith();
+            foreach (Citoyen oCitoyen in newCitoyenB)
+                AddCotoye(oCitoyen);
+        }
+        public void AddCotoye(Citoyen newCitoyen)
+        {
+            if (newCitoyen == null)
+                return;
+
+            if (cotoye != null)
+            {
+                cotoye.Add(newCitoyen);
+                if (newCitoyen.cotoye != null)
+                {
+                    newCitoyen.cotoye.Add(this);
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Key = {cin} is not found.");
+            }
+
+        }
+        public void RemoveBeenInContact(Citoyen oldCitoyen)
+        {
+            if (oldCitoyen == null)
+                return;
+
+            graph[cin].Remove(oldCitoyen);
+        }
+        public void RemoveAllBeenInContactWith()
+        {
+            graph[cin].Clear();
+        }
+
+        
+
+        private static Dictionary<string, List<Citoyen>> graph = new Dictionary<string, List<Citoyen>>();
+    }
+}
